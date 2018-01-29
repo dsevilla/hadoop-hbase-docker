d154382a6ce880482f2ae1975643944fc46a77b8
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2018-01-29 11:46:42 +0100

    Adjustments to export the correct ports and mount /tmp.

--- start-container.sh
+++ start-container.sh
@@ -1,33 +1,39 @@
-#!/bin/bash
+#! /bin/bash
 
 # run N slave containers
 tag=$1
 N=$2
+SUDO=sudo
 
-if [ $# != 2  ]
+if [ $# \< 2  ]
 then
-	echo "Set first parametar as image version tag(e.g. 0.1) and second as number of nodes"
+	echo "Set first parametar as image version tag(e.g. 0.1) and second as number of nodes. Optional third parameter indicates NOT sudo."
 	exit 1
 fi
 
+if [ $# -eq 3 ]
+then
+	SUDO=
+fi
+
 # delete old master container and start new master container
-docker rm -f master.krejcmat.com &> /dev/null
+$SUDO docker rm -f master.krejcmat.com &> /dev/null
 echo "start master container..."
-docker run -d -t --restart=always --dns 127.0.0.1 -p 9090:9090 -P --name master.krejcmat.com -h master.krejcmat.com -w /root krejcmat/hadoop-hbase-master:$tag&> /dev/null
+$SUDO docker run --rm  -d -t --restart=always --dns 127.0.0.1 -p 60010:60010 -p 50070:50070 -p 9090:9090 -p 2022:22 -P -v /tmp:/root/tmp --name master.krejcmat.com -h master.krejcmat.com -w /root krejcmat/hadoop-hbase-master:$tag&> /dev/null
 
 # get the IP address of master container
-FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master.krejcmat.com)
+FIRST_IP=$($SUDO docker inspect --format="{{.NetworkSettings.IPAddress}}" master.krejcmat.com)
 
 # delete old slave containers and start new slave containers
 i=1
 while [ $i -le $N ]
 do
-	docker rm -f slave$i.krejcmat.com &> /dev/null
+	$SUDO docker rm -f slave$i.krejcmat.com &> /dev/null
 	echo "start slave$i container..."
-	docker run -d -t --restart=always --dns 127.0.0.1 -P --name slave$i.krejcmat.com -h slave$i.krejcmat.com -e JOIN_IP=$FIRST_IP krejcmat/hadoop-hbase-slave:$tag &> /dev/null
+	$SUDO docker run -d -t --restart=always --dns 127.0.0.1 -P --name slave$i.krejcmat.com -h slave$i.krejcmat.com -e JOIN_IP=$FIRST_IP krejcmat/hadoop-hbase-slave:$tag &> /dev/null
 	((i++))
 done
 
 
 # create a new Bash session in the master container
-docker exec -it master.krejcmat.com bash
+$SUDO docker exec -it master.krejcmat.com bash
